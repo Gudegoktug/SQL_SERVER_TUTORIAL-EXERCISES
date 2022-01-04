af63887ad4f13d7d74afeaab883a7ea180a20fd4
Göktuğ Güde <96791926+Gudegoktug@users.noreply.github.com>
2022-01-04 19:49:48 +0300

    Update general.sql

--- general.sql
+++ general.sql
@@ -131,3 +131,14 @@ count(first_name) as employees
 from employees
 group by job_id
 having job_id between 3 and 8
+
+--NULL FUNCTION
+--I am gonna make an example for how can you create function 
+
+go
+  create function agecalculator(@Birthdate datetime)
+    returns int 
+        begin 
+           return datediff(year,@Birthdate,getdate())
+              end
+go
