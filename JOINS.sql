b0e450317d0daaa53674ff57a4816c869cece995
Göktuğ Güde <96791926+Gudegoktug@users.noreply.github.com>
2022-01-04 19:36:50 +0300

    Update JOINS.sql

--- JOINS.sql
+++ JOINS.sql
@@ -12,3 +12,19 @@ select *
 from Employees e
 join jobs j on e.job_id = j.job_id
 join dependents d on d.employee_id = e.employee_id
+
+--The LEFT JOIN keyword returns all records from the left table (table1), and the matching records from the right table (table2).
+--The result is 0 records from the right side, if there is no match.
+
+select CompanyName , OrderId
+from Customers
+left join Orders on Orders.CustomerId = Customer.CustomerId                --in this code, its showing it which customers dont order.
+where OrderId is null                                              
+                                                                             
+--The RIGHT JOIN keyword returns all records from the right table (table2), and the matching records from the left table (table1).
+--The result is 0 records from the left side, if there is no match.
+
+select CompanyName , OrderId
+from Orders
+right join Customers on Orders.CustomerId = Customer.CustomerId                --in this code, its showing it which customers dont order.
+where OrderId is null       
